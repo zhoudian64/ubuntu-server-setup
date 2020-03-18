44f4f268c88291f7916efe7136c58a20d4fbbdcf
Dian Zhou <1104127978@qq.com>
2020-03-18 11:00:24 +0800

    Update apt-k8s-aliyun.sh

--- apt-k8s-aliyun.sh
+++ apt-k8s-aliyun.sh
@@ -1,10 +1,9 @@
-sudo apt-get update 
-sudo apt-get install -y apt-transport-https
+apt-get update 
+apt-get install -y apt-transport-https
 curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add - 
-sudo mv /etc/apt/sources.list.d/kubernetes.list /etc/apt/sources.list.d/kubernetes.list.bak
-sudo cat << EOF > /etc/apt/sources.list.d/kubernetes.list
+mv /etc/apt/sources.list.d/kubernetes.list /etc/apt/sources.list.d/kubernetes.list.bak
+cat << EOF > /etc/apt/sources.list.d/kubernetes.list
 deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
 EOF
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
